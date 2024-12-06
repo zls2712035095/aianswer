2f04dca86741bafea034efaa7d924e8bc6095577
zls5600 <2712035095@qq.com>
2024-12-06 21:33:25 +0800

    初始化完成、完成了两个通用布局：BasicLayout、UserLayout

---
+++ aianswer-frontend/src/main.ts
@@ -0,0 +1,9 @@
+import { createApp } from "vue";
+import App from "./App.vue";
+import ArcoVue from "@arco-design/web-vue";
+
+import "@arco-design/web-vue/dist/arco.css";
+import router from "./router";
+import "@/access";
+
+createApp(App).use(ArcoVue).use(router).mount("#app");
